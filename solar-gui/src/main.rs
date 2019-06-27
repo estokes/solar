df81bdc2e8b94487feb7269ad292f3673c4e5c77
Eric Stokes <letaris@gmail.com>
2019-06-27 17:38:09 -0400

    implementing websocket

--- solar-gui/src/main.rs
+++ solar-gui/src/main.rs
@@ -1,11 +1,52 @@
-use actix_web::{middleware, web, App, HttpRequest, HttpServer};
+use actix::prelude::*;
 use actix_files;
+use actix_web::{middleware, web, App, Error, HttpRequest, HttpResponse, HttpServer};
+use actix_web_actors::ws;
 use morningstar::prostar_mppt::Stats;
 use solar_client::{load_config, send_query, FromClient, ToClient};
 use std::{
     sync::{Arc, RwLock},
     thread,
+    time::Duration,
 };
+
+static STATS_INTERVAL: Duration = Duration::from_secs(1);
+
+struct ControlSocket {
+    stats: Arc<RwLock<Option<Stats>>>,
+}
+
+impl Actor for ControlSocket {
+    type Context = ws::WebsocketContext<Self>;
+
+    fn started(&mut self, ctx: &mut Self::Context) {
+        let stats = self.stats.clone();
+        ctx.run_interval(STATS_INTERVAL, move |act, ctx| {
+            match *stats.read().unwrap() {
+                None => (),
+                Some(ref s) => ctx.text(format!("{}", s)),
+            }
+        });
+    }
+}
+
+impl StreamHandler<ws::Message, ws::ProtocolError> for ControlSocket {
+    fn handle(&mut self, msg: ws::Message, ctx: &mut Self::Context) {
+        match msg {
+            ws::Message::Ping(m) => ctx.pong(&m),
+            ws::Message::Close(_) => ctx.stop(),
+            ws::Message::Pong(_)
+            | ws::Message::Text(_)
+            | ws::Message::Binary(_)
+            | ws::Message::Nop => (),
+        }
+    }
+}
+
+fn control_socket(r: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
+    let d = r.app_data::<Arc<RwLock<Option<Stats>>>>().unwrap();
+    ws::start(ControlSocket { stats: d.clone() }, &r, stream)
+}
 
 fn read_stats(stats: Arc<RwLock<Option<Stats>>>) {
     let config = load_config(None);
@@ -34,7 +75,8 @@ fn main() -> std::io::Result<()> {
                     .header("LOCATION", "/static/index.html")
                     .finish()
             })))
-            .service(actix_files::Files::new("/static/", "/static/"))
+            .service(actix_files::Files::new("/static/", "./static/"))
+            .service(web::resource("/ws/").route(web::get().to(control_socket)))
     })
     .bind("0.0.0.0:8080")?
     .run()
--- solar-gui/static/ui.js
+++ solar-gui/static/ui.js
@@ -1,13 +1,14 @@
-$(
-    function loop() {
-	var con = new WebSocket('ws://' + window.location.host + '/ws');
-	$('#status').text = 'Connecting';
-	con.onopen = function {
-	    $('#status').text = 'Connected ' + con.protocol;
-	};
-	con.onmessage = function(e) {
-	    $('#stats').html = e.data;
-	};
-	con.onclose = loop;
-    }
-)
+function loop() {
+    var con = new WebSocket('ws://' + window.location.host + '/ws');
+    $('#status').text = 'Connecting';
+    con.onopen = function() {
+	$('#status').text = 'Connected ' + con.protocol;
+    };
+    con.onmessage = function(e) {
+	$('#stats').html = e.data;
+    };
+    con.onclose = loop;
+    con.onerror = loop;
+};
+
+loop();
