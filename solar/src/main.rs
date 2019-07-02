7b4ea4e916cb7c9111909817d816688d6189761d
Eric Stokes <letaris@gmail.com>
2019-07-02 13:25:31 -0400

    implementing summary

--- solar/src/main.rs
+++ solar/src/main.rs
@@ -25,7 +25,7 @@ use chrono;
 use daemonize::Daemonize;
 use morningstar::error as mse;
 use morningstar::prostar_mppt as ps;
-use solar_client::{self, Config, FromClient, ToClient};
+use solar_client::{self, Config, FromClient, Stats, ToClient};
 use std::{
     fs,
     io::{self, LineWriter, Write},
@@ -146,10 +146,112 @@ fn run_server(config: Config) {
     }
 }
 
-fn file_exists(path: &Path) -> bool {
+macro_rules! avg {
+    ($r0:ident, $r1:ident, $( $fld:ident ),*) => {
+        $(
+            $r0.$fld = ($r0.$fld + $r1.$fld) / 2.;
+        )*
+    }
+}
+
+macro_rules! max {
+    ($r0:ident, $r1:ident, $( $fld: ident ),*) => {
+        $(
+            $r0.$fld = max($r0.fld, $r1.$fld);
+        )*
+    }
+}
+
+fn stats_accum(acc: &mut Stats, s: &Stats) {
+    use std::cmp::{max, min};
+    acc.battery_v_min_daily = min(acc.battery_v_min_daily, s.battery_v_min_daily);
+    max!(
+        acc,
+        s,
+        timestamp,
+        software_version,
+        battery_voltage_settings_multiplier,
+        heatsink_temperature,
+        battery_temperature,
+        ambient_temperature,
+        rts_temperature,
+        u_inductor_temperature,
+        v_inductor_temperature,
+        w_inductor_temperature,
+        array_faults,
+        ah_charge_resettable,
+        ah_charge_total,
+        kwh_charge_resettable,
+        kwh_charge_total,
+        load_faults,
+        ah_load_resettable,
+        ah_load_total,
+        hourmeter,
+        alarms,
+        battery_v_max_daily,
+        ah_charge_daily,
+        ah_load_daily,
+        array_faults_daily,
+        load_faults_daily,
+        alarms_daily,
+        array_voltage_max_daily,
+        charge_state,
+        load_state
+    );
+    avg!(
+        acc,
+        s,
+        supply_3v3,
+        supply_12v,
+        supply_5v,
+        gate_drive_voltage,
+        battery_terminal_voltage,
+        array_voltage,
+        load_voltage,
+        charge_current,
+        array_current,
+        load_current,
+        battery_current_net,
+        battery_sense_voltage,
+        meterbus_voltage,
+        battery_voltage_slow,
+        target_voltage,
+        lvd_setpoint,
+        array_power,
+        array_vmp,
+        array_max_power_sweep,
+        array_voc,
+        array_voltage_fixed,
+        array_voc_percent_fixed
+    );
+}
+
+#[derive(Debug)]
+struct UnexpectedObjectKind;
+
+impl std::error::Error for UnexpectedObjectKind {}
+impl std::fmt::Display for UnexpectedObjectKind {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+        write!(fmt, "expected a file, found something else")
+    }
+}
+
+fn file_exists(path: &Path) -> Result<bool, Box<dyn std::error::Error>> {
     match fs::metadata(&todays) {
-        Ok(m) => m.is_file(),
-        Error(e) => e.kind = std::io::ErrorKind::NotFound,
+        Ok(m) => {
+            if m.is_file() {
+                Ok(true)
+            } else {
+                Error(Box::new(UnexpectedObjectKind))
+            }
+        }
+        Error(e) => {
+            if e.kind = std::io::ErrorKind::NotFound {
+                Ok(false)
+            } else {
+                Error(Box::new(e))
+            }
+        }
     }
 }
 
@@ -157,7 +259,7 @@ fn rotate_log(cfg: &Config) {
     use libflate::gzip::Encoder;
     use std::{fs::OpenOptions, io};
     let todays = cfg.archive_for_date(chrono::Utc::today());
-    if file_exists(&todays) {
+    if file_exists(&todays).expect("error checking file exists") {
         println!("nothing to do");
     } else {
         let current = cfg.log_file();
@@ -177,7 +279,8 @@ fn rotate_log(cfg: &Config) {
         io::copy(
             &mut fs::File::open(&tmp).expect("failed to open tmp file"),
             &mut encoder,
-        ).expect("failed to copy tmp file to the archive");
+        )
+        .expect("failed to copy tmp file to the archive");
         fs::remove_file(&tmp).expect("failed to remove tmp file");
     }
 }
