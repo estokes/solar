a65dcb6f248b43bf3eb5150ba37b35419c3a6b25
Eric Stokes <letaris@gmail.com>
2019-07-13 18:24:20 -0400

    implement history

--- solar-gui/src/main.rs
+++ solar-gui/src/main.rs
@@ -1,14 +1,59 @@
 #[macro_use]
 extern crate serde_derive;
+use chrono::prelude::*;
 use actix::prelude::*;
 use actix_files;
 use actix_web::{middleware, web, App, Error, HttpRequest, HttpResponse, HttpServer};
 use actix_web_actors::ws;
-use solar_client::{load_config, send_query, FromClient, Stats, ToClient};
+use libflate::gzip;
+use solar_client::{load_config, send_query, FromClient, Stats, ToClient, Config};
 use std::{
+    error, fs, io,
+    path::{Path, PathBuf},
     sync::{Arc, RwLock},
     thread,
 };
+
+#[derive(Clone)]
+struct AppData {
+    stats: Arc<RwLock<Option<Stats>>>,
+    config: Config,
+}
+
+fn read_history_file(file: &Path, v: &mut Vec<Stats>) -> Result<(), Box<dyn error::Error>> {
+    use serde_json::error::Category;
+    let reader: Box<dyn io::Reader> = {
+        let mut f = fs::File::open(file)?;
+        if file.extension != "gz" {
+            f.into()
+        } else {
+            gzip::Decoder::new(f)?.into()
+        }
+    };
+    let buf = io::BufReader::new(reader);
+    loop {
+        match serde_json::from_reader::<Stats>(&buf) {
+            Ok(o) => v.push(o),
+            Err(e) => match e.classify() {
+                Category::Io | Category::Eof => return Ok(()),
+                Category::Syntax | Category::Data => return Err(e).into()
+            }
+        }
+    }
+}
+
+fn read_history(cfg: &Config, mut days: i64) -> Result<Vec<Stats>, Box<dyn error::Error>> {
+    let mut v = Vec::new();
+    let today = Local::today();
+    while days > 0 {
+        for date in today.checked_sub_signed(Duration::days(days)).iter() {
+            read_history_file(&cfg.archive_for_date(date).one_minute_averages, &mut v)?
+        }
+        days -= 1;
+    }
+    read_history_file(&cfg.log_file(), &mut v)?;
+    Ok(v)
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 enum Target {
@@ -24,6 +69,7 @@ enum ToBrowser {
     CmdErr(String),
     Stats(Stats),
     Status(Target, bool),
+    EndOfHistory,
 }
 
 impl ToBrowser {
@@ -37,14 +83,12 @@ impl ToBrowser {
 
 #[derive(Debug, Serialize, Deserialize)]
 enum FromBrowser {
-    StatsHistory { start: String, end: String },
+    StatsHistory(i64),
     StatsCurrent,
     Set(Target, bool),
 }
 
-struct ControlSocket {
-    stats: Arc<RwLock<Option<Stats>>>,
-}
+struct ControlSocket(AppData);
 
 impl Actor for ControlSocket {
     type Context = ws::WebsocketContext<Self>;
@@ -58,15 +102,30 @@ impl StreamHandler<ws::Message, ws::ProtocolError> for ControlSocket {
             ws::Message::Pong(_) | ws::Message::Binary(_) | ws::Message::Nop => (),
             ws::Message::Text(m) => match serde_json::from_str::<FromBrowser>(&m) {
                 Err(e) => ctx.text(
-                    ToBrowser::CmdErr(format!("invalid command: {},  {}", m, e.to_string()))
-                        .enc(),
+                    ToBrowser::CmdErr(format!(
+                        "invalid command: {},  {}",
+                        m,
+                        e.to_string()
+                    ))
+                    .enc(),
                 ),
                 Ok(cmd) => match cmd {
-                    FromBrowser::StatsCurrent => match *self.stats.read().unwrap() {
+                    FromBrowser::StatsCurrent => match *self.0.stats.read().unwrap() {
                         None => ctx.text(ToBrowser::CmdErr("not available".into()).enc()),
                         Some(s) => ctx.text(ToBrowser::Stats(s).enc()),
                     },
-                    FromBrowser::StatsHistory { .. } | FromBrowser::Set(_, _) => {
+                    FromBrowser::StatsHistory(days) => {
+                        match read_history(&self.appdata, days) {
+                            Err(e) => ctx.text(ToBrowser::CmdErr(e.to_string()).enc()),
+                            Ok(history) => {
+                                for o in history {
+                                    ctx.text(ToBrowser::Stats(o).enc())
+                                }
+                                ctx.text(ToBrowser::EndOfHistory.enc())
+                            }
+                        }
+                    }
+                    FromBrowser::Set(_, _) => {
                         ctx.text(ToBrowser::CmdErr("not implemented".into()).enc())
                     }
                 },
@@ -76,17 +135,16 @@ impl StreamHandler<ws::Message, ws::ProtocolError> for ControlSocket {
 }
 
 fn control_socket(r: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
-    let d = r.app_data::<Arc<RwLock<Option<Stats>>>>().unwrap();
-    ws::start(ControlSocket { stats: d.clone() }, &r, stream)
-}
+    let d = r.app_data::<AppData>().unwrap();
+    ws::start(ControlSocket(d.clone()), &r, stream)
+p}
 
-fn read_stats(stats: Arc<RwLock<Option<Stats>>>) {
-    let config = load_config(None);
-    let iter = send_query(&config, FromClient::TailStats).expect("failed to tail stats");
+fn read_stats(appdata: AppData) {
+    let iter = send_query(&appdata.config, FromClient::TailStats).expect("failed to tail stats");
     thread::spawn(move || {
         for s in iter {
             match s {
-                ToClient::Stats(s) => *stats.write().unwrap() = Some(s),
+                ToClient::Stats(s) => *appdata.stats.write().unwrap() = Some(s),
                 ToClient::Settings(_) | ToClient::Ok | ToClient::Err(_) => (),
             }
         }
@@ -97,10 +155,10 @@ fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
     HttpServer::new(|| {
-        let stats: Arc<RwLock<Option<Stats>>> = Arc::new(RwLock::new(None));
-        read_stats(stats.clone());
+        let appdata = AppData { stats: Arc::new(RwLock::new(None)), config: load_config(None) };
+        read_stats(appdata.clone());
         App::new()
-            .data(stats)
+            .data(appdata)
             .wrap(middleware::Logger::default())
             .service(web::resource("/").route(web::get().to(|| {
                 HttpResponse::Found()
