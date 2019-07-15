f1ff0a446197340b1ce6ef4b23e20b223d9c50f9
Eric Stokes <letaris@gmail.com>
2019-07-15 14:02:26 -0400

    added the 4 major charts

--- solar-gui/static/ui.js
+++ solar-gui/static/ui.js
@@ -80,21 +80,35 @@ function update_charts(stats) {
 	window.chartChargeCurrentCfg.data.datasets[0].data.push({
 	    x: ts,
 	    y: e.V0.charge_current
-	})
+	});
 	window.chartAhChargeCfg.data.datasets[0].data.push({
 	    x: ts,
 	    y: amp_seconds_to_amp_hours(e.V0.ah_charge_daily)
-	})
+	});
+	window.chartBatteryVoltageCfg.data.datasets[0].data.push({
+	    x: ts,
+	    y: e.V0.battery_sense_voltage
+	});
+	window.chartArrayPowerCfg.data.datasets[0].data.push({
+	    x: ts,
+	    y: e.V0.array_power
+	});
     })
     window.chartChargeCurrent.update();
     window.chartAhCharge.update();
+    window.chartBatteryVoltage.update();
+    window.chartArrayPower.update();
 }
 
 function init_charts() {
     window.chartChargeCurrentCfg = empty_chart_config('Charge Current', 'Amps');
     window.chartChargeCurrent = new Chart($('#current_chart'), window.chartChargeCurrentCfg);
-    window.chartAhChargeCfg = empty_chart_config('Ah Charge', "Ah");
+    window.chartAhChargeCfg = empty_chart_config('Ah Charge', 'Ah');
     window.chartAhCharge = new Chart($('#ah_chart'), window.chartAhChargeCfg);
+    window.chartBatteryVoltageCfg = empty_chart_config('Battery Voltage', 'Volts');
+    window.chartBatteryVoltage = new Chart($('#battery_voltage_chart'), window.chartBatteryVoltageCfg);
+    window.chartArrayPowerCfg = empty_chart_config('Array Power', 'Watts');
+    window.chartArrayPower = new Chart($('#array_power_chart'), window.chartArrayPowerCfg);
 }
 
 var con = null;
@@ -102,18 +116,34 @@ var con = null;
 function loop() {
     var receiving_history = false;
     var history = [];
-    var iid = 0;
+    var stats_iid = 0;
+    var chart_iid = 0;
+
+    var reload = function(msg) {
+	$('#status').text(msg);
+	console.log("connection was closed");
+	window.clearInterval(stats_iid);
+	window.clearInterval(chart_iid);
+	con = null;
+	loop();
+    };
 
     if(con != null) return;
     else con = new WebSocket('ws://' + window.location.host + '/ws/');
     
     $('#status').text('Connecting');
     con.onopen = function() {
-	$('#status').text('Connected ' + con.protocol);
+	$('#status').text('Loading History');
 	receiving_history = true;
 	con.send('{"StatsHistory": 3}');
-	iid = window.setInterval(() => { if(!receiving_history) con.send('"StatsCurrent"'); }, 5000);
 	init_charts();
+	stats_iid = window.setInterval(() => { if(!receiving_history) con.send('"StatsCurrent"'); }, 5000);
+	chart_iid = window.setInterval(() => {
+	    if(!receiving_history) {
+		update_charts(history);
+		history = [];
+	    }
+	}, 600000);
     };
     con.onmessage = function(e) {
 	var v = JSON.parse(e.data);
@@ -121,33 +151,23 @@ function loop() {
 	else if (v.hasOwnProperty('CmdErr')) {
 	    console.log(v);
 	} else if (v.hasOwnProperty('Stats')) {
-	    if(receiving_history) { history.push(v.Stats); }
-	    else {
-		display_stats(v.Stats);
-		update_charts([v.Stats]);
-	    }
+	    history.push(v.Stats);
+	    if(!receiving_history) display_stats(v.Stats);
 	} else if (v.hasOwnProperty('Status')) {
 	    console.log(v);
 	} else if (v == 'EndOfHistory') {
+	    $('#status').text('Connected');
 	    receiving_history = false;
 	    update_charts(history);
+	    history = [];
 	} else {
 	    console.log("unknown response from server: " + v);
 	}
     };
-    con.onclose = function() {
-	$('#status').text("Not Connected");
-	console.log("connection was closed");
-	window.clearInterval(iid);
-	con = null;
-	loop();
-    };
+    
+    con.onclose = function() { reload("Disconnected"); }
     con.onerror = function(e) {
-	$('#status').text("Disconnected " + e);
-	console.log("connection was closed " + e);
-	window.clearInterval(iid);
-	con = null;
-	loop();
+	reload("Disconnected " + e);
     };
 };
 
